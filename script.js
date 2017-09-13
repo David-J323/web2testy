21ca4c4a2c23d02e91ad2d2d6de6c8fdc6d2ae9e
David-J323 <david.jaramillo323@yahoo.com>
2017-09-13 16:40:03 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
 console.log('hello M8!')
 document.write('<h1> Hi!</h1>')
+let words = [cool, stupid, poor]; 
+console.log("Johnny is very" + " " + words[Math.floor(words.length*Math.random())] + ". I am " +  
+            words[Math.floor(words.length*Math.random())] + ". On a scale of 1-10 I would be a " + 
+            prompt("choose a number between 1 and 10") + ".") 
